ea87139e029ff14210c1f03869848c7ebe3ada11
Silver Alcid <silveralcid@gmail.com>
2025-10-08 06:28:13 -0400

    chore(settings): add django-cors-headers and enable CORS for local development

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -42,12 +42,14 @@ INSTALLED_APPS = [
     'rest_framework',
     'django_filters',
     'drf_spectacular',
+    'corsheaders'
 
     # Local Apps
     'jobs',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -129,3 +131,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# CORS Development Configuration
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+
+
+# CORS Production Configuration
+# CORS_ALLOWED_ORIGINS = [
+#     "https://yourfrontend.com",
+#     "https://staging.yourfrontend.com",
+# ]
