7b5a613816c53e302924728a18bba4570913588b
Silver Alcid <silveralcid@gmail.com>
2025-10-08 08:12:23 -0400

    feat(api): add CompanyViewSet with nested jobs endpoint and DRF routing

--- backend/jobs/views.py
+++ backend/jobs/views.py
@@ -1,14 +1,23 @@
-from django.shortcuts import render
 from rest_framework import viewsets
-from .models import Company, Job
-from .serializers import CompanySerializer, JobSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from .models import Job, Company
+from .serializers import JobSerializer, CompanySerializer
+
+
+class JobViewSet(viewsets.ModelViewSet):
+    queryset = Job.objects.select_related("company").all()
+    serializer_class = JobSerializer
 
 
 class CompanyViewSet(viewsets.ModelViewSet):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
 
-
-class JobViewSet(viewsets.ModelViewSet):
-    queryset = Job.objects.select_related("company").all()
-    serializer_class = JobSerializer
+    # /api/companies/<uuid:id>/jobs/
+    @action(detail=True, methods=["get"])
+    def jobs(self, request, pk=None):
+        company = self.get_object()
+        jobs = Job.objects.filter(company=company)
+        serializer = JobSerializer(jobs, many=True)
+        return Response(serializer.data)
